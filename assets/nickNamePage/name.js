c5304140ad0f038df655cf3a2c7d5d1ef48cb15c
Max3796 <gurpreet.singh@kalvium.community>
2023-11-04 18:11:17 +0530

    html and css

---
+++ assets/nickNamePage/name.js
@@ -0,0 +1,4 @@
+let playName = document.getElementById("play")
+playName.addEventListener("click",()=>{
+   window.location.href="../instructions/ins.html"
+})
---
+++ main.js
@@ -0,0 +1,8 @@
+let playNow = document.getElementById("play-now")
+console.log("playNow: ", playNow);
+
+
+playNow.addEventListener("click" , nickName )
+function nickName(){
+    window.location.href="./assets/nickNamePage/name.html"
+}
